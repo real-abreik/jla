14078699266d8d6e3833a65c3ff42c58fbc67aa7
Adam Breitkreutz <abreik2@gmail.com>
2024-07-21 19:39:08 +0000

    move discord bot to prod

--- scriptrunner/src/jla.php
+++ scriptrunner/src/jla.php
@@ -5,9 +5,13 @@ error_reporting(E_ALL);
 $start = time();
 $elTeamId = 66261;
 $cwTeamId = 'jla2024';
+$checkWalk = true;
+//$webhook = "https://discord.com/api/webhooks/1264139508790853682/ULxQ1NLym0AvN9bupVa7UMxa9DurtmOX1CMZyNgUsemTCfnRUQsyooSmXIGMa-MsFhZZ";
+$webhook = "https://discordapp.com/api/webhooks/1264291547273367634/YVBqSW7c6GD3n1Q6Sk-Vg0wC8ic2aTUH7smZWpty_porcckOlG9B8AYVzcwf3FmvVeRo";
 
 $redis = new Redis();
 $redis->connect('redis', 6379);
+
 
 $redis_key = 'jla';
 
@@ -30,10 +34,13 @@ foreach ($el_don as $donation) {
 	if (in_array($donation->donationID, $state->extralife)) {
 		break;
 	}
-	$state->extralife[] = $donation->donationID;
 	if (empty($state->extralife) || $state->timestamp == $start || $state->timestamp < ($start - 1800)) {
+		echo "EL Donation Fault\n";
+		echo json_encode($state) . "\n";
+		echo "Timestamp: {$start}\n";
 		continue;
 	}
+	$state->extralife[] = $donation->donationID;
 
 	$amount = "$" . number_format($donation->amount);
 	$message = "{$donation->recipientName} received a {$amount} donation for Extra Life";
@@ -45,9 +52,8 @@ foreach ($el_don as $donation) {
 		$filtered = str_replace(["\n", "\r"], ' ', $filtered);
 		$message .= " with the message:\n> {$filtered}";
 	}
-	echo "Extra Life Donation Found: {$message}\n";
+	echo "Extra Life Donation Found {$donation->donationID}: {$message}\n";
 
-	$webhook = "https://discord.com/api/webhooks/1264139508790853682/ULxQ1NLym0AvN9bupVa7UMxa9DurtmOX1CMZyNgUsemTCfnRUQsyooSmXIGMa-MsFhZZ";
 	$data = [
 		'username' => "Extra Life Donation Alert",
 		'avatar_url' => 'https://ctxfoundation.bswhealth.com/image/Extra-life-bug.png',
@@ -61,56 +67,57 @@ foreach ($el_don as $donation) {
 	$result = curl_exec($ch);
 }
 
-echo "CHOC Walk Lookup\n";
-$cw_html = file_get_contents("https://www.chocwalk.org/{$cwTeamId}");
-$raised_raw = get_string_between($cw_html, '<span class="was-raised">', '</span>');
-$raised = filter_var($raised_raw, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-$previousRaised = $state->chocwalk->raised;
-if ($raised > 0 && $raised > $previousRaised) {
-	$state->chocwalk->raised = $raised;
-}
-$increase = $raised - $previousRaised;
-if ($raised > 0 && $previousRaised > 0 && $increase > 0) {
-	$recent_raw = get_string_between($cw_html, 'new RecentDonationsVue(', ').mount(');
-	$recent = false;
-	if (!empty($recent_raw)) {
-		$recent = json_decode($recent_raw);
+if ($checkWalk) {
+	echo "CHOC Walk Lookup\n";
+	$cw_html = file_get_contents("https://www.chocwalk.org/{$cwTeamId}");
+	$raised_raw = get_string_between($cw_html, '<span class="was-raised">', '</span>');
+	$raised = filter_var($raised_raw, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+	$previousRaised = $state->chocwalk->raised;
+	if ($raised > 0 && $raised > $previousRaised) {
+		$state->chocwalk->raised = $raised;
 	}
-	$previousDonation = 0;
-	$previousName = '';
-	if (!empty($recent->recentDonations)) {
-		$lastDonation = $recent->recentDonations[0];
-		if ($lastDonation->amount > 0) {
-			$previousDonation = $lastDonation->amount;
+	$increase = $raised - $previousRaised;
+	if ($raised > 0 && $previousRaised > 0 && $increase > 0) {
+		$recent_raw = get_string_between($cw_html, 'new RecentDonationsVue(', ').mount(');
+		$recent = false;
+		if (!empty($recent_raw)) {
+			$recent = json_decode($recent_raw);
 		}
-		if (!empty($lastDonation->donor->name)) {
-			$previousName = $lastDonation->donor->name;
-		} else if ($lastDonation->donor->anonymous) {
-			$previousName = "Anonymous";
+		$previousDonation = 0;
+		$previousName = '';
+		if (!empty($recent->recentDonations)) {
+			$lastDonation = $recent->recentDonations[0];
+			if ($lastDonation->amount > 0) {
+				$previousDonation = $lastDonation->amount;
+			}
+			if (!empty($lastDonation->donor->name)) {
+				$previousName = $lastDonation->donor->name;
+			} else if ($lastDonation->donor->anonymous) {
+				$previousName = "Anonymous";
+			}
 		}
-	}
-	if ($previousDonation == $increase && !empty($previousName)) {
-		$message = "Received a $" . number_format($increase) . " donation to CHOC Walk from {$previousName}";
-	} else {
-		$message = "CHOC Walk donation total increased by $" . number_format($increase) . " and the CHOC Walk team total is now $" . number_format($raised) . " thanks to everyone's generous donations.";
-	}
-	echo "CHOC Walk Donation Found: {$message}\n";
+		if ($previousDonation == $increase && !empty($previousName)) {
+			$message = "Received a $" . number_format($increase) . " donation to CHOC Walk from {$previousName}";
+		} else {
+			$message = "CHOC Walk donation total increased by $" . number_format($increase) . " and the CHOC Walk team total is now $" . number_format($raised) . " thanks to everyone's generous donations.";
+		}
+		echo "CHOC Walk Donation Found: {$message}\n";
 
-	$webhook = "https://discord.com/api/webhooks/1264139508790853682/ULxQ1NLym0AvN9bupVa7UMxa9DurtmOX1CMZyNgUsemTCfnRUQsyooSmXIGMa-MsFhZZ";
-	$data = [
-		'username' => "CHOC Walk Donation Alert",
-		'avatar_url' => 'https://choc.org/wp-content/uploads/2021/03/cropped-CHOC_Favicon_512px-180x180-1.png',
-		'content' => $message,
-	];
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $webhook);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$result = curl_exec($ch);
-
+		$data = [
+			'username' => "CHOC Walk Donation Alert",
+			'avatar_url' => 'https://choc.org/wp-content/uploads/2021/03/cropped-CHOC_Favicon_512px-180x180-1.png',
+			'content' => $message,
+		];
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $webhook);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$result = curl_exec($ch);
+	}
 }
 
+$state->timestamp = $start;
 $redis->set($redis_key, json_encode($state));
 sleep(400);
 
